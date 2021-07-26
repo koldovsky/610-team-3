3495b30455e6331bc50ee2414dd04e5c0e59bf2b
Olha Romaniv <86737567+OlhaRomaniv@users.noreply.github.com>
2021-07-26 14:53:03 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -78,12 +78,6 @@ function updateClock() {
     clockFoot.innerText = new Date().toLocaleTimeString();
 }
 setInterval(updateClock, 1000);
-/*
-//page loading
-$(window).on("load", function() {
-    $(".loader-wrapper").fadeOut("slow");
-});
-*/
 
 // Quotes 
 const quotesSlides = [
